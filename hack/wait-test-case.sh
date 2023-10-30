4475357a1874cb27a87785b778ef9063e400376a
Jorge Turrado <jorge.turrado@scrm.lidl>
2023-10-30 21:04:27 +0100

    fix: Use correct api for waiting test cases
    Signed-off-by: Jorge Turrado <jorge.turrado@scrm.lidl>

--- hack/wait-test-case.sh
+++ hack/wait-test-case.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-status=$(kubectl get k6 -n $1 k6-case -o jsonpath="{.status.stage}")
+status=$(kubectl get testrun -n $1 k6-case -o jsonpath="{.status.stage}")
 while [[ "$status" != "finished" && 
             "$status" != "stopped" &&
             "$status" != "error" ]]
 do
 	echo "Test in progress, current status: $status"
 	sleep 30
-	status=$(kubectl get k6 -n $1 k6-case -o jsonpath="{.status.stage}")
+	status=$(kubectl get testrun -n $1 k6-case -o jsonpath="{.status.stage}")
 done
 
 echo "Test finished with status $status"
